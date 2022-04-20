a2bec97255d8b3dfec75e05529eb866313af5a3c
Ninroot-Eater <jamesthiha@Jamess-MacBook-Pro.local>
2022-04-20 22:57:36 +0800

    signin view fixed fr

--- duwin_backend/views.py
+++ duwin_backend/views.py
@@ -39,17 +39,16 @@ class SignIn(APIView):
             )
         res = requests.get(
             "https://graph.microsoft.com/v1.0/me",
-            headers={"Authorization": f"Bearer {token}", "Content-Type": "application/json"}
+            headers={"Authorization": f"Bearer {token.strip()}", "Content-Type": "application/json"}
             )
 
         if res.status_code not in range(199,300):
             return Response({"MS_error": res.json()}, status=res.status_code)
-
         res = res.json()
         user = User.objects.filter(ms_id=res["id"]).first()
 
         if user is None:
-            user = User.objects.create(email=res["email"], ms_id=res["id"])
+            user = User.objects.create(email=res["mail"], ms_id=res["id"])
             user.save()
 
         access = RefreshToken.for_user(user).access_token
